c6c8b25dc0f6f30c36887d83d21bdda2eff2ef84
Evan Tetzlaff <evantetzlaff@Evans-MacBook.local>
2016-07-17 20:03:48 -0600

    something something darkside

--- install.sh
+++ install.sh
@@ -4,18 +4,27 @@ xcode-select --install
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Get brew servies
+brew tap homebrew/services
+
+# Brews I might want
+brew install postgresql
+brew install mysql
+
 # Install RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 
 # Install Neovim
 brew install neovim
+
+# Dein plugin management
+mkdir -p ~/.config/nvim/
+# mkdir -p ~/.config/nvim/dein/repos/github.com/Shougo/dein.vim
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh -s ~/.config/nvim/dein
+
+# Neovim Symlinks
 ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 cp ~/dotfiles/fonts/* ~/Library/fonts/.
-
-mkdir -p ~/.config/nvim/
-mkdir -p ~/.config/nvim/dein/repos/github.com/Shougo/dein.vim
-
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh -s ~/.config/nvim/dein
 
 # Ordinary VIM
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
