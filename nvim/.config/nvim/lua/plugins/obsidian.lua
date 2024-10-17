463af00c45a7d7d977dcd24e721148c493a15ebf
Evan Tetzlaff <reuhssurance@gmail.com>
2024-10-17 15:49:59 -0600

    Better obsidian

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -28,12 +28,64 @@ return {
     },
     config = function()
       require('obsidian').setup({
-        workspaces = {
-          {
-            name = 'personal',
-            path = '~/vaults/personal',
-          },
+        -- workspaces = {
+        --   {
+        --     name = 'personal',
+        --     path = '~/vaults/personal',
+        --   },
+        -- },
+        dir = '~/main-vault',
+        completion = {
+          nvim_cmp = true,
+          min_chars = 3,
         },
+        daily_notes = {
+          folder = 'daily',
+          date_format = '%Y-%m-%d',
+          template = 'daily.md',
+        },
+        notes_subdir = 'inbox',
+        new_notes_location = 'notes_subdir',
+        templates = {
+          folder = 'templates',
+          date_format = '%Y-%m-%d',
+          time_format = '%H:%M',
+        },
+        ui = {
+          enabled = false,
+        },
+        note_id_func = function(title)
+          local suffix = ''
+          if title ~= nil then
+            suffix = title:gsub(' ', '-'):gsub('[A-Za-z0-9-', ''):lower()
+          else
+            for _ = 1, 4 do
+              suffix = suffix .. string.char(math.random(65, 90))
+            end
+          end
+          return tostring(os.date('%Y-%m-%d')) .. '-' .. suffix
+        end,
+        note_frontmatter_func = function(note)
+          if note.title then
+            note:add_alias(note.title)
+          end
+
+          local today = os.date('%Y-%m-%d')
+          note:add_tag(today)
+
+          local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+          if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+            for k, v in pairs(note.metadata) do
+              out[k] = v
+            end
+          end
+
+          return out
+        end,
+        follow_url_func = function(url)
+          vim.fn.jobstart({ 'open', url })
+        end,
       })
     end,
   },
