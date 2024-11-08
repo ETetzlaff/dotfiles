69e252756c181c12599aefa9133cb304e05435e7
Evan Tetzlaff <reuhssurance@gmail.com>
2024-11-08 11:26:57 -0700

    Adding golang plugin and autopairs

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,8 +1,4 @@
 require('lazy').setup({
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  'easymotion/vim-easymotion', -- ,,w tokenizer
-  'preservim/nerdcommenter', -- easy commenting maps
-
   require 'plugins/lsp',
   require 'plugins/treesitter',
   require 'plugins/telescope',
@@ -14,6 +10,12 @@ require('lazy').setup({
   require 'plugins/harpoon',
   require 'plugins/lualine',
   require 'plugins/git',
+  require 'plugins/autopairs',
+
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'easymotion/vim-easymotion', -- ,,w tokenizer
+  'preservim/nerdcommenter', -- easy commenting maps
+  'ray-x/go.nvim', -- golang
 }, {
     ui = {
       -- If you are using a Nerd Font: set icons to an empty table which will use the
@@ -36,3 +38,14 @@ require('lazy').setup({
     },
   }
 )
+
+-- Go auto fmt
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimports()
+  end,
+  group = format_sync_grp,
+})
+require("go").setup()
